fa988f2db745cfd801c21d6feba923f4c115ee99
Zayne Fitch <fitchzayne@gmail.com>
2026-06-07 10:51:43 -0400

    Update leaders.js

--- leaders.js
+++ leaders.js
@@ -1,1 +1,86 @@
-
+const leaders = [
+{
+    section: "Command Team",
+    name: "[Insert name here]",
+    title: "[Insert title here]",
+    image: "images/commander.jpg",
+    bio: "[Insert bio here]"
+},
+{
+    section: "Command Team",
+    name: "[Insert name here]",
+    title: "[Insert title here]",
+    image: "images/commander.jpg",
+    bio: "[Insert bio here]"
+},
+{
+    section: "Command Team",
+    name: "[Insert name here]",
+    title: "[Insert title here]",
+    image: "images/commander.jpg",
+    bio: "[Insert bio here]"
+},
+{
+    section: "Command Team",
+    name: "[Insert name here]",
+    title: "[Insert title here]",
+    image: "images/commander.jpg",
+    bio: "[Insert bio here]"
+},
+{
+    section: "HHC",
+    name: "[Insert name here]",
+    title: "[Insert title here]",
+    image: "images/commander.jpg",
+    bio: "[Insert bio here]"
+},
+{
+    section: "HHC",
+    name: "[Insert name here]",
+    title: "[Insert title here]",
+    image: "images/commander.jpg",
+    bio: "[Insert bio here]"
+},
+{
+    section: "A Co",
+    name: "[Insert name here]",
+    title: "[Insert title here]",
+    image: "images/commander.jpg",
+    bio: "[Insert bio here]"
+},
+{
+    section: "A Co",
+    name: "[Insert name here]",
+    title: "[Insert title here]",
+    image: "images/commander.jpg",
+    bio: "[Insert bio here]"
+},
+{
+    section: "B Co",
+    name: "[Insert name here]",
+    title: "[Insert title here]",
+    image: "images/commander.jpg",
+    bio: "[Insert bio here]"
+},
+{
+    section: "B Co",
+    name: "[Insert name here]",
+    title: "[Insert title here]",
+    image: "images/commander.jpg",
+    bio: "[Insert bio here]"
+},
+{
+    section: "C Co",
+    name: "[Insert name here]",
+    title: "[Insert title here]",
+    image: "images/commander.jpg",
+    bio: "[Insert bio here]"
+},
+{
+    section: "C Co",
+    name: "[Insert name here]",
+    title: "[Insert title here]",
+    image: "images/commander.jpg",
+    bio: "[Insert bio here]"
+}
+];
