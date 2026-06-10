3ad33c14233c434320b768edc3d87e4b31d67900
Zayne Fitch <fitchzayne@gmail.com>
2026-06-10 10:41:29 -0400

    Update leaders.js
    Added new sections, and names/title for each leader that will be a part of the bio site

--- leaders.js
+++ leaders.js
@@ -1,85 +1,85 @@
 const leaders = [
 {
-    section: "Command Team",
-    name: "[Insert name here]",
-    title: "[Insert title here]",
+    section: "State",
+    name: "BG Andrew W. Collins",
+    title: "TAAG - Army",
     image: "images/commander.jpg",
     bio: "[Insert bio here]"
 },
 {
-    section: "Command Team",
-    name: "[Insert name here]",
-    title: "[Insert title here]",
+    section: "58th EMIB",
+    name: "COL Kirstie Y. Roy",
+    title: "CDR, 58th EMIB",
     image: "images/commander.jpg",
     bio: "[Insert bio here]"
 },
 {
-    section: "Command Team",
-    name: "[Insert name here]",
-    title: "[Insert title here]",
+    section: "628th IEW BN",
+    name: "LTC Asa C. Coates",
+    title: "CDR, 628th IEW BN]",
     image: "images/commander.jpg",
     bio: "[Insert bio here]"
 },
 {
-    section: "Command Team",
-    name: "[Insert name here]",
-    title: "[Insert title here]",
+    section: "628th IEW BN",
+    name: "SGM Johnathan X. McGlone",
+    title: "Acting CSM, 628th IEW BN",
     image: "images/commander.jpg",
     bio: "[Insert bio here]"
 },
 {
-    section: "HHC",
-    name: "[Insert name here]",
-    title: "[Insert title here]",
+    section: "HHC, 628th IEW BN",
+    name: "CPT Matthew M. Melcher",
+    title: "CDR, HHC, 628th IEW BN",
     image: "images/commander.jpg",
     bio: "[Insert bio here]"
 },
 {
-    section: "HHC",
-    name: "[Insert name here]",
-    title: "[Insert title here]",
+    section: "HHC, 628th IEW BN",
+    name: "1SG Gavin B. Beem",
+    title: "1SG, HHC, 628th IEW BN",
     image: "images/commander.jpg",
     bio: "[Insert bio here]"
 },
 {
-    section: "A Co",
-    name: "[Insert name here]",
-    title: "[Insert title here]",
+    section: "A CO, 628th IEW BN",
+    name: "CPT Shelbie L. Settle",
+    title: "CDR, A CO, 628th IEW BN",
     image: "images/commander.jpg",
     bio: "[Insert bio here]"
 },
 {
-    section: "A Co",
-    name: "[Insert name here]",
-    title: "[Insert title here]",
+    section: "A CO, 628th IEW BN",
+    name: "1SG Scott D. Valent",
+    title: "1SG, A CO, 628th IEW BN",
     image: "images/commander.jpg",
     bio: "[Insert bio here]"
 },
 {
-    section: "B Co",
-    name: "[Insert name here]",
-    title: "[Insert title here]",
+    section: "B CO, 628th IEW BN",
+    name: "CPT Angela M. Monfiletto",
+    title: "CDR, B CO, 628th IEW BN",
     image: "images/commander.jpg",
     bio: "[Insert bio here]"
 },
 {
-    section: "B Co",
-    name: "[Insert name here]",
-    title: "[Insert title here]",
+    section: "B CO, 628th IEW BN",
+    name: "SSG Michael B. Beck",
+    title: "Acting DET NCOIC, 628th IEW BN",
     image: "images/commander.jpg",
     bio: "[Insert bio here]"
 },
 {
-    section: "C Co",
-    name: "[Insert name here]",
-    title: "[Insert title here]",
+    section: "C CO, 628th IEW BN",
+    name: "CPT Joshua C. Bowman",
+    title: "CDR, C CO, 628th IEW BN",
     image: "images/commander.jpg",
     bio: "[Insert bio here]"
 },
 {
-    section: "C Co",
-    name: "[Insert name here]",
-    title: "[Insert title here]",
+    section: "C CO, 628th IEW BN",
+    name: "1SG Akobi S. Burton",
+    title: "1SG, C CO, 628th IEW BN",
     image: "images/commander.jpg",
     bio: "[Insert bio here]"
 }
