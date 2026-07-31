30698cc61564991e2f4a28574813c465c43e2c21
Zayne Fitch <fitchzayne@gmail.com>
2026-07-31 12:38:47 -0400

    Update leaders.js

--- leaders.js
+++ leaders.js
@@ -18,21 +18,49 @@ const leaders = [
     name: "LTC Asa C. Coates",
     title: "CDR, 628th IEW BN",
     image: "images/commander.jpg",
-    bio: "[Insert bio here]"
+    bio: `Born in Washington, D.C., Lieutenant Colonel Asa C. Coates entered military service in 2000 as a Motor Transport Operator (88M) in the Maryland Army National Guard (MDARNG).  He soon joined Morgan State University’s Reserve Officers’ Training Corps and enrolled in the Simultaneous Membership Program, remaining a member of the MDARNG.  In May of 2004, Lieutenant Colonel Coates earned a Bachelor of Science in Marketing and commissioned as a Second Lieutenant in the Quartermaster Corps (Distinguished Military Graduate). He served as a Platoon Leader in the 243rd Engineer Company, before transferring to the Massachusetts Army National Guard where he served as a Platoon Leader, Water Operations Officer, and Acting Commander in the 125th Quartermaster Company.  He returned to the MDARNG and served as S3 for the 1297th   Combat Sustainment Support Battalion (CSSB) while simultaneously serving as the MDARNG Officer Strength Manager within the Recruiting and Retention Battalion. 
+
+Lieutenant Colonel Coates then entered the Active Guard Reserve Program as Assistant S3, 58th Troop Command.  He deployed to Kandahar, Afghanistan and “Supported the Battle” with the 1297th CSSB, as S3, during Consolidation III and Transition I of Operation Enduring Freedom (OEF).  Upon return from deployment, he served as the 58th Troop Command as Assistant S4 and Headquarters Company Commander.  In May of 2015, he transitioned to Secretary of the General Staff, followed by an assignment as Aide de Camp / Executive Officer (XO) for The Adjutant General. In 2018, Lieutenant Colonel Coates transitioned to the role of XO for the 110th Information Operations Battalion while simultaneously preparing to deploy to Camp Lemonnier, Djibouti to support OEF where he served as the CJ39 in addition to the CJ2 (interim) for Combined Joint Task Force – Horn of Africa.  Upon returning from deployment, he transitioned to S2 / Assistant S3 at the 58th Expeditionary Military Intelligence Brigade before movin to his current assignment as the MDARNG G2.  Leadership selected Lieutenant Colonel Coates to command the 628th Intelligence and Electronic Warfare Battalion (“Blackjacks”) in September of 2024.  
+
+Lieutenant Colonel Coates’ military education includes Quartermaster Officer Basic, Logistics Advanced Career Course, Officer Strength Manager Course, Theater Sustainment Course, Support Operations Course, Military Intelligence Career Course, Military Deception Planner’s Course, Joint Targeting Staff Course, Joint Information Operations Course, and an array of Information and Cyber related schools and courses.  Lieutenant Colonel Coates completed Intermediate Level Education and holds the Functional Area 30A.  He is a graduate of the Joint Forces Staff College Joint Combined Warfighting School.  He holds skill identifiers for Lean Six Sigma, Joint Qualified Officer, Special Technical Operations Chief, and Parachutist. He obtained a Master of Arts in Business Management and Leadership from Liberty University in 2022.  
+
+Lieutenant Colonel Coates’ awards and decorations include the Bronze Star Medal, Defense Meritorious Service Medal, Meritorious Service Medal (1 bronze OLC), Army Commendation Medal (1 silver OLC), Army Achievement Medal, and many other awards and decorations. 
+
+Lieutenant Colonel Coates and his wife Me’Shell reside in Howard County, MD with their children, Mikayla (18), Aiden (18), Jasmine (12), and Ava (6).`
 },
 {
     section: "628th IEW BN",
     name: "SGM Johnathan X. McGlone",
     title: "Acting CSM, 628th IEW BN",
     image: "images/commander.jpg",
-    bio: "[Insert bio here]"
+    bio: `Born in Norfolk, Virginia, Sergeant Major Johnathan McGlone began his military career in 2003 by enlisting in the Maryland Army National Guard as a culinary specialist. After graduating from Basic Combat Training and Advanced Individual Training, he was assigned to the 1-175th Infantry Battalion, where he quickly demonstrated his leadership potential. In 2009, he was promoted to Culinary Management NCO for HHC 1-224th Aviation. While with this unit, he deployed to Kosovo in 2013 as part of Task Force Phalanx, providing critical logistical support to a MEDEVAC unit from the Oregon National Guard in support of peacekeeping missions.
+
+In 2016, Sergeant Major McGlone deployed with the historic 29th Infantry Division to Zarqa, Jordan, where he served as the Deputy Mayor of the Joint Training Center, a multinational base housing U.S. and allied forces. Upon returning, his leadership trajectory continued with his selection in 2021 as the First Sergeant for Maryland's largest Army National Guard unit, the 729th Quartermaster Company. Following this key assignment, he was promoted to Command Sergeant Major for the 1297th Combat Support Sustainment Battalion (CSSB) in 2024, where he was instrumental in leading soldiers to "Support the Battle."
+
+Beyond his traditional military roles, Sergeant Major McGlone has made significant contributions in other areas. He currently serves at the National Guard Bureau in the G-1 (HRP-S) section as the Assistant Program Manager for Military Funeral Honors. In this capacity, he assists in managing a $65 million program budget, 382 GSA vehicles, and over 550 ADOS personnel across 54 states and territories. Previously, as the State Military Funeral Honors Training NCOIC, he personally performed over 1,800 military funeral honors missions and trained more than 500 personnel from various military branches in the proper execution of these solemn duties.
+
+Sergeant Major McGlone's commitment to professional development is evident in his extensive military education, which includes the Sergeants Major Academy, Senior Enlisted Joint Professional Military Education, and the Joint Staff Action Officer course.
+
+His distinguished service has been recognized with numerous awards and decorations, including the Meritorious Service Medal (2), Army Commendation Medal (3), and the Army Achievement Medal (8).
+
+Sergeant Major McGlone lives in Baltimore, Maryland, with his wife, Steffanie, and their sons, Gabriel (15), Jonathan (15), and Jaxon (11).`
 },
 {
     section: "HHC, 628th IEW BN",
     name: "CPT Matthew M. Melcher",
     title: "CDR, HHC, 628th IEW BN",
     image: "images/commander.jpg",
-    bio: "[Insert bio here]"
+    bio: `Captain Mathew Melcher was born in Mesa, Arizona and enlisted in the United States Army in September 2001 as an Arabic Cryptologic Linguist. Upon completion of Language School and Advanced Individual Training, he was assigned to 10th Special Forces Group (Airborne). Following completion of the Special Forces Qualification Course, he served as a Special Forces Medical Sergeant with 5th Special Forces Group (Airborne) and later joined the Maryland Army National Guard and was assigned to 20th Special Forces Group (Airborne).
+
+Captain Melcher received his commission as a Military Intelligence Officer, and he served as a Signal Intelligence Platoon Leader with the 629th Expeditionary-Military Intelligence Battalion. His subsequent assignments have included Political-Military Analyst with the Europe and Eurasia Center in the Defense Intelligence Agency and Senior Instructor and Train, Advise, and Counsel (TAC) Officer with the Maryland Army National Guard Officer Candidate School.
+
+Captain Melcher holds an Associate of Arts in Arabic from the Defense Language Institute, a Bachelor of Arts in History from the University of Maryland, Baltimore County, a Master of Arts in Security Studies from Georgetown University, and a Graduate Certificate in Strategic Warning Intelligence from the National Intelligence University.
+
+His military education includes the Special Forces Qualification Course, the Maryland Army National Guard Officer Candidate School, the Military Intelligence Basic Officer Leader Course, and the Military Intelligence Captains Career Course.
+
+Captain Melcher’s awards and decorations include the Afghanistan Campaign Medal, Iraq Campaign Medal, and National Defense Service Medal.
+
+Captain Melcher and his wife, Captain Anna Hagan, an Army aviation officer, live in Maryland with their three daughters, Matilda (14), Junia (12), and Josephine (4).`
 },
 {
     section: "HHC, 628th IEW BN",
@@ -46,7 +74,15 @@ const leaders = [
     name: "CPT Shelbie L. Settle",
     title: "CDR, A CO, 628th IEW BN",
     image: "images/commander.jpg",
-    bio: "[Insert bio here]"
+    bio: `Born in Clinton, Maryland, Captain Settle entered military service in 2011 with the Simultaneous Membership Program and was assigned to the 2nd Brigade Special Troops Battalion, Pennsylvania Army National Guard (PAARNG) as an Officer Candidate. In May of 2014, Captain Settle graduated from the Pennsylvania State University, with a Bachelor of Science in Biological Anthropology. Upon commissioning into the PAARNG as a Second Lieutenant in the Military Intelligence Corps, she was assigned to Bravo Company, 2nd Brigade Special Troops Battalion as the Signals Intelligence Platoon Leader. 
+
+Captain Settle served in the 2nd Brigade Special Troops Battalion from 2014 to 2019 as Signals Intelligence Platoon Leader, Human Intelligence Platoon Leader, and acting Executive Officer. In 2019, she transferred to the Maryland Army National Guard and served in the Maryland Joint Staff J2. In 2020, Captain Settle entered State Active Duty during the COVID-19 pandemic where she supported Maryland’s pandemic response in the Unclassified Processing, Assessment, and Dissemination cell within the J2. Upon the completion of the COVID-19 response, she was selected for an Active Duty Operational Support tour with the Defense Intelligence Agency where she served as the Military Officer for the Middle East and Africa Regional Center Counterintelligence team for three years. In May 2025, she assumed command of Alpha Company, 628th Intelligence and Electronic Warfare (IEW) Battalion.
+
+Captain Settle’s military education includes the Military Intelligence Officer Basic Course, Military Intelligence Captain’s Career Course, Military Capabilities Assessment Course, among other technical intelligence and security courses. Her civilian education includes a Bachelor of Science from the Pennsylvania State University in 2014, and she is currently pursuing a Master of Science in Biodefense from George Mason University.
+
+Captain Settle’s awards and decorations include the Joint Service Commendation Medal, Army Commendation Medal, Air Force Achievement Medal, among other awards. She is also a recipient of the German Armed Forces Proficiency Badge – Gold.
+
+Captain Settle and her partner Dale reside in Alexandria, Virginia.`
 },
 {
     section: "A CO, 628th IEW BN",
@@ -70,21 +106,43 @@ const leaders = [
     name: "CPT Angela M. Monfiletto",
     title: "CDR, B CO, 628th IEW BN",
     image: "images/commander.jpg",
-    bio: "[Insert bio here]"
+    bio: `Born in Staten Island, New York, Captain Monfiletto entered military service in 2015 with the Simultaneous Membership Program and was assigned to Alpha Company, 629th Expeditionary Military Intelligence Battalion (EMIBn), Maryland Army National Guard (MDARNG) as an Officer Candidate. In May 2018, Captain Monfiletto graduated as a Distinguished Military Graduate from Loyola University Maryland’s Reserve Officer Training Corps with a Bachelor of Arts in Speech Language and Hearing Sciences and a minor in History. Upon commissioning into the Maryland Army National Guard as a Second Lieutenant in the Military Intelligence Corps, he was assigned to Alpha Company, 629th EMIBn as the Human Intelligence (HUMINT) Platoon Leader. 
+
+Captain Monfiletto served in the 629th EMIBn from 2018 to 2021 and deployed with the unit to Kandahar, Afghanistan and Al Udeid Air Base, Quatar in support of United States Forces-Afghanistan (USFOR-A) under Operation Resolute Support from 2020-2021 as a Platoon Leader. Upon return from deployment, Captain Monfiletto served as the Current Operations Deputy for Maryland’s Vaccine Equity Task Force (VETF) to support COVID vaccine operations. She also served as a Platoon Trainer with Alpha Company, 1st Battalion, 70th Regional Training Institute to support MDARNG’s Officer Candidate School (OCS). In 2023, Captain Monfiletto was selected to serve as the Senior Platoon Trainer for MDARNG’s OCS program. In September 2025, he assumed command of Bravo Company, 628th Intelligence and Electronic Warfare (IEW) Battalion. 
+
+Captain Monfiletto’s military education includes the Military Intelligence Officer Basic Course, Military Intelligence Captain’s Career Course, U.S. Army Common Faculty Development – Instructor Course (CFD-IC), Maneuver Tactics Foundation Course (MTFC), and Army Platoon Trainer Qualification Course (PTQC). Her  civilian education includes a Bachelor of Arts from Loyola University Maryland in 2018 and a Doctor of Audiology from Towson University in 2025. `
 },
 {
     section: "B CO, 628th IEW BN",
     name: "SSG Michael B. Beck",
     title: "Acting DET NCOIC, 628th IEW BN",
     image: "images/commander.jpg",
-    bio: "[Insert bio here]"
+    bio: `Born in Baltimore, Maryland, Staff Sergeant Michael B. Beck entered military service in 2014 as a Signals Intelligence Analyst (35N) in the United States Army. His first assignment was with the 704th Military Intelligence Battalion at Fort Meade, Maryland, where he served as a Critic Reporter and Quality Control Analyst supporting national intelligence missions.
+
+Staff Sergeant Beck later transferred to the 116th Military Intelligence Battalion at Fort Gordon, Georgia, where he served as a Mission Manager and Collector supporting the Guardrail mission focused on the United States European Command (EUCOM) Area of Responsibility. Following this assignment, he returned to Fort Meade, Maryland, and was assigned to the 780th Military Intelligence Battalion in support of United States Cyber Command. During this time, he served as a Senior Target Analyst Reporter (TAR) and Technical Digital Network Analyst (TDNA) on the 100th Combat Support Team (CST), providing critical intelligence support to cyber and national security operations.
+
+While assigned to the 780th Military Intelligence Battalion, Staff Sergeant Beck deployed to Mogadishu, Somalia, in support of Naval Special Warfare and Special Operations Command East Africa. During the deployment, he served as a Threat Analyst and Targeting Analyst supporting SEAL Team Two and joint special operations missions throughout the region.
+
+After more than ten years of active-duty service, Staff Sergeant Beck departed active duty in December 2024 and joined the Maryland Army National Guard. He currently serves as the Noncommissioned Officer in Charge (NCOIC) of Bravo Detachment, 628th Intelligence and Electronic Warfare Battalion.
+
+Staff Sergeant Beck’s military awards and decorations include the Navy and Marine Corps Achievement Medal, Army Commendation Medal, Army Achievement Medal, Meritorious Unit Commendation, and numerous other awards and decorations.
+
+Staff Sergeant Beck and his wife, Alicia, reside in Baltimore County, Maryland, with their daughter, Braylee (11), and their two dogs, Cannoli and Marlie.`
 },
 {
     section: "C CO, 628th IEW BN",
     name: "CPT Joshua C. Bowman",
     title: "CDR, C CO, 628th IEW BN",
     image: "images/commander.jpg",
-    bio: "[Insert bio here]"
+    bio: `Born in Columbia, South Carolina, Captain Bowman entered military service in 2015 as an Infantryman in the South Carolina Army National Guard (SCARNG). Shortly after enlisting, CPT Bowman was selected for the Simultaneous Membership Program and assigned to the 678th Air Defense Artillery Brigade, SCARNG as an Officer Candidate. In May of 2018, Captain Bowman graduated as a Distinguished Military Graduate from The Citadel, the Military College of South Carolina, with a Bachelor of Arts in International Affairs and a Minor in Intelligence and Homeland Security. Upon commissioning into the Maryland Army National Guard (MDARNG) as a Second Lieutenant in the Military Intelligence Corps, he was assigned to Bravo Company, 629th Expeditionary Military Intelligence Battalion (EMIBn) as the Multifunction Platoon Leader. 
+
+Captain Bowman served in the 629th EMIBn from 2018 to 2021 as Multi-Function Platoon Leader, Processing, Exploitation, and Dissemination (PED) Platoon Leader, and acting Executive Officer. Additionally, he served as Task Force Executive Officer for the 2020 Presidential Election and Inauguration State Active Duty mission. In 2021, Captain Bowman deployed to Syria as Detachment Commander for the 629th EMIBn Signals Intelligence (SIGINT) Detachment in support of a Special Operations Joint Task Force during Operation Inherent Resolve. Upon return from deployment in 2022, he served as the SIGINT Officer for the 58th Expeditionary Military Intelligence Brigade (EMIB). In 2024, Captain Bowman was selected to serve as the MDARNG Deputy G2 and entered Active Duty service with the MDARNG. In September 2025, he assumed command of Charlie Company, 628th Intelligence and Electronic Warfare (IEW) Battalion.
+
+Captain Bowman’s military education includes the Military Intelligence Officer Basic Course, Military Intelligence Captain’s Career Course, Signals Intelligence and Electronic Warfare Officer’s Course, Army Open-Source Intelligence Fundamentals Course, Unit Movement Officer and Deployment Planners Course, among other technical intelligence and security courses. His civilian education includes a Bachelor of Arts from the Citadel in 2018, and he is currently pursuing a Master of Divinity from Trinity Evangelical Divinity School.
+
+Captain Bowman’s awards and decoration include the Defense Meritorious Service Medal, Army Commendation Device with ‘C’ Device and one silver oar leaf cluster, and Army Achievement Medal with two bronze oak leaf clusters, among other awards. He is also a recipient of the German Armed Forces Proficiency Badge – Gold.
+
+Captain Bowman and his wife Alyssa reside in Fort Washington, Maryland.`
 },
 {
     section: "C CO, 628th IEW BN",
